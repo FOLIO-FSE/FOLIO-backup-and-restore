b6e3dad84112a383cea14806a4af0e02c3561652
Theodor Tolstoy <ttolstoy@ebsco.com>
2020-04-14 17:15:37 +0200

    prolonged chalmers covid-19 lockdown

--- service_requests_tools/extend_open_loans.py
+++ service_requests_tools/extend_open_loans.py
@@ -20,51 +20,56 @@ def add_stats(stats, a):
 def put_loan(folio_client, loan):
     url = f"{folio_client.okapi_url}/circulation/loans/{loan['id']}"
     print(url)
-    req = requests.put(url,
-        headers=folio_client.okapi_headers,
-        data=json.dumps(loan))
+    req = requests.put(url, headers=folio_client.okapi_headers, data=json.dumps(loan))
     print(req.status_code)
-    req.raise_for_status() 
+    req.raise_for_status()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("okapi_url",
-                    help=("url of your FOLIO OKAPI endpoint."
-                          "See settings->software version in FOLIO"))
-parser.add_argument("tenant_id",
-                    help=("id of the FOLIO tenant. "
-                          "See settings->software version in FOLIO"))
+parser.add_argument(
+    "okapi_url",
+    help=(
+        "url of your FOLIO OKAPI endpoint." "See settings->software version in FOLIO"
+    ),
+)
+parser.add_argument(
+    "tenant_id",
+    help=("id of the FOLIO tenant. " "See settings->software version in FOLIO"),
+)
 parser.add_argument("username", help=("the api user"))
 parser.add_argument("password", help=("the api users password"))
 args = parser.parse_args()
-print(f'{args.tenant_id} {args.username} {args.okapi_url}')
-folio_client = FolioClient(args.okapi_url, args.tenant_id, args.username,
-                           args.password)
+print(f"{args.tenant_id} {args.username} {args.okapi_url}")
+folio_client = FolioClient(args.okapi_url, args.tenant_id, args.username, args.password)
 patron_emails = set()
-path1 = '/circulation/loans'
-query = '?query=(status.name="Open" AND dueDate < "2020-04-10*")'
+path1 = "/circulation/loans"
+query = '?query=(status.name="Open" AND dueDate < "2020-05-14*")'
 print(f"path: {path1} query: {query}")
 i = 0
 stats = {}
 failed = list()
-loans = folio_client.folio_get_all(path1, 'loans', query)
+loans = folio_client.folio_get_all(path1, "loans", query)
 print(len(loans))
 for loan in loans:
     loan_to_put = copy.deepcopy(loan)
-    del loan_to_put['metadata']
-    loan_to_put['dueDate'] = '2020-04-10T22:59:59.000+0000'
-    print(loan_to_put['dueDate'])
+    del loan_to_put["metadata"]
+    loan_to_put["dueDate"] = "2020-05-14T21:59:59.000+0000"
+    print(loan_to_put["dueDate"])
     try:
         put_loan(folio_client, loan_to_put)
-        print(f"PUT /circulation/loans/{loan_to_put['id']}\t{json.dumps(loan_to_put)}\t{loan}]")
-        patron = folio_client.folio_get_single_object(f"/users/{loan['userId']}")
-        patron_emails.add(patron['personal']['email'])
+        print(
+            f"PUT /circulation/loans/{loan_to_put['id']}\t{json.dumps(loan_to_put)}\t{loan}]"
+        )
+        # patron = folio_client.folio_get_single_object(f"/users/{loan['userId']}")
+        # patron_emails.add(patron["personal"]["email"])
     except Exception as ee:
-        print(f"ERROR! PUT /circulation/loans/{loan_to_put['id']}\t{json.dumps(loan_to_put)}\t{loan}]")
+        print(
+            f"ERROR! {i} PUT /circulation/loans/{loan_to_put['id']}\t{json.dumps(loan_to_put)}\t{loan}]"
+        )
         print(ee)
         traceback.print_exc()
         raise ee
-    i+=1
+    i += 1
 print(i)
 
 print(patron_emails)
